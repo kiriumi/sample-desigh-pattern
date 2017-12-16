e3cecf5afddf167886883c1ea4e63645c3f0f147
Kengo Mukai <kengo@sample.com>
2017-12-16 17:58:38 +0900

    Visitorにメソッドを追加

--- src/main/java/component_visitor/ComponentVisitor2.java
+++ src/main/java/component_visitor/ComponentVisitor2.java
@@ -2,11 +2,19 @@ package component_visitor;
 
 public class ComponentVisitor2 implements IfComponentVisitable {
 
-	public void visit(AcceptableComposite1 composite) {
-		visit(composite);
+	public void visit(AcceptableComposite0 composite0) {
+		visit(composite0);
 	}
 
-	public void visit(AcceptableLeaf1 leaf) {
-		visit(leaf);
+	public void visit(AcceptableComposite1 composite1) {
+		visit(composite1);
+	}
+
+	public void visit(AcceptableLeaf1 leaf1) {
+		visit(leaf1);
+	}
+
+	public void visit(AcceptableLeaf2 leaf2) {
+		visit(leaf2);
 	}
 }
