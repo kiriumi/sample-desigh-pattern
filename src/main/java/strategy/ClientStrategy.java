45aeb4a5d815536ae036cb9817abd877175bf394
Kengo Mukai <kengo@sample.com>
2017-12-17 02:20:01 +0900

    Strategyパターンの見直し

--- src/main/java/strategy/ClientStrategy.java
+++ src/main/java/strategy/ClientStrategy.java
@@ -6,26 +6,10 @@ public class ClientStrategy {
 
 	public static void main(String[] args) {
 
-		//		int[] keys = { 1, 2, 3 };
-		//		IntStream keysStream = Arrays.stream(keys);
-		//
-		//		keysStream.forEach(key -> {
-		//
-		//			System.out.print("key=" + key + ": ");
-		//
-		//			IfStrategy strategy = null;
-		//			strategy = (key == 1)
-		//					? new Strategy1()
-		//					: new Strategy2();
-		//
-		//			strategy.execute();
-		//		});
-
 		IfStrategy[] stategys = { new Strategy1(), new Strategy2() };
 		Stream<IfStrategy> stream = Stream.of(stategys);
 
 		stream.forEach(stategy -> stategy.execute());
-
 	}
 
 }
