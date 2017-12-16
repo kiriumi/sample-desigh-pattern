d445905527a881080e6e3dc4944684869faa4565
Kengo Mukai <kengo@sample.com>
2017-12-17 03:49:41 +0900

    Singletonパターンをコミット

---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package singleton;
+
+public class Singleton {
+
+	private Singleton instance;
+
+	private Singleton() {
+
+	}
+
+	public Singleton getInstance() {
+
+		this.instance = (instance == null)
+				? new Singleton()
+				: instance;
+
+		return instance;
+	}
+
+}
