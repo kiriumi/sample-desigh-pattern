c998652e5802ad5919e68305dea972c670815890
Kengo Mukai <kengo@sample.com>
2017-12-17 22:52:00 +0900

    Facadeパターンをコミット

---
+++ src/main/java/facade/ClientFacade.java
@@ -0,0 +1,10 @@
+package facade;
+
+public class ClientFacade {
+
+	public static void main(String... args) {
+
+		Facade facade = new Facade();
+		facade.execute();
+	}
+}
---
+++ src/main/java/facade/Facade.java
@@ -0,0 +1,8 @@
+package facade;
+
+public class Facade {
+
+	public void execute() {
+		System.out.println("こちらで処理を受け付けました");
+	}
+}
