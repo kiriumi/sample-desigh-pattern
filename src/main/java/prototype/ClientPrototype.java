d4d9997b68405073b025136e35b5a6f6cab5b78f
Kengo Mukai <kengo@sample.com>
2017-12-19 23:45:39 +0900

    Prototypeパターンのコミット

---
+++ src/main/java/prototype/ClientPrototype.java
@@ -0,0 +1,20 @@
+package prototype;
+
+import util.Utils;
+
+public class ClientPrototype {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+
+		IfPrototype prototype = new Prototype("Hoge");
+
+		for (int i = 1; i <= 3; i++) {
+			IfPrototype cloned = prototype.createClone();
+			cloned.execute1();
+			cloned.execute2(i);
+			Utils.printNewLine();
+		}
+
+	}
+
+}
