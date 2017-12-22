b77e054a35ccee3794aefa8bc832af8d5a340d2f
Kengo Mukai <kengo@sample.com>
2017-12-22 23:14:23 +0900

    Commandパターンを追加

---
+++ src/main/java/command/ClientCommand.java
@@ -0,0 +1,30 @@
+package command;
+
+public class ClientCommand {
+
+	public static void main(String[] args) {
+
+		Invoker invoker = new Invoker();
+
+		IfReceiver receiver1 = new Receiver1();
+		IfReceiver receiver2 = new Receiver2();
+
+		IfCommand commandA = new CommandA();
+		IfCommand commandB1 = new CommandB();
+		IfCommand commandB2 = new CommandB();
+
+		commandA.setReceiver(receiver1);
+		commandB1.setReceiver(receiver1);
+		commandB2.setReceiver(receiver2);
+
+		invoker.add(commandA);
+		invoker.add(commandB1);
+		invoker.add(commandB2);
+
+		invoker.remove(commandB1);
+
+		invoker.executeAll();
+
+	}
+
+}
---
+++ src/main/java/command/IfReceiver.java
@@ -0,0 +1,8 @@
+package command;
+
+public interface IfReceiver {
+
+	public void actionA();
+
+	public void actionB();
+}
